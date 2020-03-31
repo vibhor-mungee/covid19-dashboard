b8aeaa0c8c5581174a0126ccf28193f0600b452d
himanshu pawar <himanshu.pawar@wsisrdev.com>
2020-03-31 17:13:08 +0530

    added map

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React,{useState} from 'react';
+import { useLazyQuery } from '@apollo/react-hooks';
+import gql from "graphql-tag";
+import GoogleMapReact from 'google-map-react';
+import request from 'request';
+import { UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap';
+import './App.css';
+
+import location_mark from './image/iconfinder_gpsmapicons01_68004.png';
+
+function App() {
+  // const date = new Date();
+  // const dateFormate= `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()-1}`
+  const[latLong,setLatLng]=useState([{lat:'20',lng:'77'}]);
+  const[countryName,countryNameByLatLng]=useState('India');
+
+  const COVID_19_DATA = gql`
+  query($country:String!){
+    ReportByCountry(country:$country) {
+      id
+      country
+      flag
+      cases
+      deaths
+      recovered
+      active_cases{
+        currently_infected_patients
+        inMidCondition
+        criticalStates
+      }
+      closed_cases {
+        recovered
+        deaths
+        cases_which_had_an_outcome
+      }
+    }
+  }`
+  const [reportByCountry, { called, loading, data }] = useLazyQuery(COVID_19_DATA,{ variables: { country: countryName } });
+  const getCountryNameByLatLng= async(lat,lng)=>{
+    const latLongArray=[];
+    await latLongArray.push({lat,lng})
+    setLatLng(latLongArray);
+    await request({
+          url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyDL9qOq_Qr4kFCE651q5v5UfYGDmEEX5Oo`,
+          method: 'get',
+      }, async(e, r, body) => {
+          if (!body) {
+              return false;
+          }
+          const solve = JSON.parse(body);
+          if(solve && solve.results.length>0){
+          const getNameArray= solve.results[0].formatted_address.split(',');
+          await countryNameByLatLng(getNameArray[getNameArray.length -1].trim());
+          }
+      }
+      );
+      reportByCountry();
+  }
+  const AnyReactComponent = () => <div>
+      <img width={35} src={location_mark} alt="mark" id="UncontrolledPopover"/>
+      {console.log(called, loading,'data>>',data)}
+      {called &&data && data.ReportByCountry &&data.ReportByCountry.country[0]===countryName &&
+        <UncontrolledPopover placement="bottom" target="UncontrolledPopover">
+        <PopoverHeader>COVID19</PopoverHeader>
+        <PopoverBody>
+          <div>
+            <h6>Confirmed: {data.ReportByCountry.active_cases[0].currently_infected_patients}</h6>
+            <h6>Deaths: {data.ReportByCountry.closed_cases[0].deaths}</h6>
+          </div>
+        </PopoverBody>
+      </UncontrolledPopover>
+      }
+      </div>;
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDL9qOq_Qr4kFCE651q5v5UfYGDmEEX5Oo' }}
+          defaultCenter={{lat: 20, lng: 77}}
+          defaultZoom={5}
+          onClick={({lat,lng})=>getCountryNameByLatLng(lat,lng)}
+        >
+          <AnyReactComponent
+            lat={latLong &&latLong.length>0 && latLong[0].lat}
+            lng={latLong &&latLong.length>0 && latLong[0].lng}
+          />
+        </GoogleMapReact>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/selectedcountry.js
@@ -0,0 +1,510 @@
+export default[
+    {
+        "id":1,
+        "timezones": [
+            "Africa/Algiers"
+        ], 
+        "latlng": [
+            28, 
+            3
+        ], 
+        "name": "Algeria", 
+        "country_code": "DZ", 
+        "capital": "Algiers"
+    },
+    {
+        "id":2,
+        "timezones": [
+            "Australia/Lord_Howe", 
+            "Antarctica/Macquarie", 
+            "Australia/Hobart", 
+            "Australia/Currie", 
+            "Australia/Melbourne", 
+            "Australia/Sydney", 
+            "Australia/Broken_Hill", 
+            "Australia/Brisbane", 
+            "Australia/Lindeman", 
+            "Australia/Adelaide", 
+            "Australia/Darwin", 
+            "Australia/Perth", 
+            "Australia/Eucla"
+        ], 
+        "latlng": [
+            -27, 
+            133
+        ], 
+        "name": "Australia", 
+        "country_code": "AU", 
+        "capital": "Canberra"
+    },
+    {
+        "id":3,
+        "timezones": [
+            "Europe/Vienna"
+        ], 
+        "latlng": [
+            47.33333333, 
+            13.33333333
+        ], 
+        "name": "Austria", 
+        "country_code": "AT", 
+        "capital": "Vienna"
+    },
+    {
+        "id":4,
+        "timezones": [
+            "Europe/Brussels"
+        ], 
+        "latlng": [
+            50.83333333, 
+            4
+        ], 
+        "name": "Belgium", 
+        "country_code": "BE", 
+        "capital": "Brussels"
+    },
+    {
+        "id":5,
+        "timezones": [
+            "America/Noronha", 
+            "America/Belem", 
+            "America/Fortaleza", 
+            "America/Recife", 
+            "America/Araguaina", 
+            "America/Maceio", 
+            "America/Bahia", 
+            "America/Sao_Paulo", 
+            "America/Campo_Grande", 
+            "America/Cuiaba", 
+            "America/Santarem", 
+            "America/Porto_Velho", 
+            "America/Boa_Vista", 
+            "America/Manaus", 
+            "America/Eirunepe", 
+            "America/Rio_Branco"
+        ], 
+        "latlng": [
+            -10, 
+            -55
+        ], 
+        "name": "Brazil", 
+        "country_code": "BR", 
+        "capital": "Bras\u00edlia"
+    },
+    {
+        "id":6,
+        "timezones": [
+            "America/St_Johns", 
+            "America/Halifax", 
+            "America/Glace_Bay", 
+            "America/Moncton", 
+            "America/Goose_Bay", 
+            "America/Blanc-Sablon", 
+            "America/Toronto", 
+            "America/Nipigon", 
+            "America/Thunder_Bay", 
+            "America/Iqaluit", 
+            "America/Pangnirtung", 
+            "America/Atikokan", 
+            "America/Winnipeg", 
+            "America/Rainy_River", 
+            "America/Resolute", 
+            "America/Rankin_Inlet", 
+            "America/Regina", 
+            "America/Swift_Current", 
+            "America/Edmonton", 
+            "America/Cambridge_Bay", 
+            "America/Yellowknife", 
+            "America/Inuvik", 
+            "America/Creston", 
+            "America/Dawson_Creek", 
+            "America/Fort_Nelson", 
+            "America/Vancouver", 
+            "America/Whitehorse", 
+            "America/Dawson"
+        ], 
+        "latlng": [
+            60, 
+            -95
+        ], 
+        "name": "Canada", 
+        "country_code": "CA", 
+        "capital": "Ottawa"
+    },
+    {
+        "id":7,
+        "timezones": [
+            "Asia/Shanghai", 
+            "Asia/Urumqi"
+        ], 
+        "latlng": [
+            35, 
+            105
+        ], 
+        "name": "China", 
+        "country_code": "CN", 
+        "capital": "Beijing"
+    },
+    {
+        "id":8,
+        "timezones": [
+            "Asia/Hong_Kong"
+        ], 
+        "latlng": [
+            22.267, 
+            114.188
+        ], 
+        "name": "Hong Kong", 
+        "country_code": "HK", 
+        "capital": "City of Victoria"
+    },
+    {
+        "id":9,
+        "timezones": [
+            "Europe/Copenhagen"
+        ], 
+        "latlng": [
+            56, 
+            10
+        ], 
+        "name": "Denmark", 
+        "country_code": "DK", 
+        "capital": "Copenhagen"
+    },
+    {
+        "id":10,
+        "timezones": [
+            "Europe/Paris"
+        ], 
+        "latlng": [
+            46, 
+            2
+        ], 
+        "name": "France", 
+        "country_code": "FR", 
+        "capital": "Paris"
+    },
+    {
+        "id":11,
+        "timezones": [
+            "Europe/Berlin", 
+            "Europe/Busingen"
+        ], 
+        "latlng": [
+            51, 
+            9
+        ], 
+        "name": "Germany", 
+        "country_code": "DE", 
+        "capital": "Berlin"
+    },
+    {
+        "id":12,
+        "timezones": [
+            "Europe/Athens"
+        ], 
+        "latlng": [
+            39, 
+            22
+        ], 
+        "name": "Greece", 
+        "country_code": "GR", 
+        "capital": "Athens"
+    },
+    {
+        "id":13,
+        "timezones": [
+            "Asia/Kolkata"
+        ], 
+        "latlng": [
+            20, 
+            77
+        ], 
+        "name": "India", 
+        "country_code": "IN", 
+        "capital": "New Delhi"
+    },
+    {
+        "id":14,
+        "timezones": [
+            "Asia/Jakarta", 
+            "Asia/Pontianak", 
+            "Asia/Makassar", 
+            "Asia/Jayapura"
+        ], 
+        "latlng": [
+            -5, 
+            120
+        ], 
+        "name": "Indonesia", 
+        "country_code": "ID", 
+        "capital": "Jakarta"
+    },
+    {
+        "id":15,
+        "timezones": [
+            "Asia/Tehran"
+        ], 
+        "latlng": [
+            32, 
+            53
+        ], 
+        "name": "Iran", 
+        "country_code": "IR", 
+        "capital": "Tehran"
+    },
+    {
+        "id":16,
+        "timezones": [
+            "Asia/Baghdad"
+        ], 
+        "latlng": [
+            33, 
+            44
+        ], 
+        "name": "Iraq", 
+        "country_code": "IQ", 
+        "capital": "Baghdad"
+    },
+    {
+        "id":17,
+        "timezones": [
+            "Europe/Dublin"
+        ], 
+        "latlng": [
+            53, 
+            -8
+        ], 
+        "name": "Ireland", 
+        "country_code": "IE", 
+        "capital": "Dublin"
+    },
+    {
+        "id":18,
+        "timezones": [
+            "Asia/Jerusalem"
+        ], 
+        "latlng": [
+            31.47, 
+            35.13
+        ], 
+        "name": "Israel", 
+        "country_code": "IL", 
+        "capital": "Jerusalem"
+    },
+    {
+        "id":19,
+        "timezones": [
+            "Europe/Rome"
+        ], 
+        "latlng": [
+            42.83333333, 
+            12.83333333
+        ], 
+        "name": "Italy", 
+        "country_code": "IT", 
+        "capital": "Rome"
+    },
+    {
+        "id":20,
+        "timezones": [
+            "Asia/Kuala_Lumpur", 
+            "Asia/Kuching"
+        ], 
+        "latlng": [
+            2.5, 
+            112.5
+        ], 
+        "name": "Malaysia", 
+        "country_code": "MY", 
+        "capital": "Kuala Lumpur"
+    },
+    {
+        "id":21,
+        "timezones": [
+            "Europe/Amsterdam"
+        ], 
+        "latlng": [
+            52.5, 
+            5.75
+        ], 
+        "name": "Netherlands", 
+        "country_code": "NL", 
+        "capital": "Amsterdam"
+    },
+    {
+        "id":22,
+        "timezones": [
+            "Europe/Oslo"
+        ], 
+        "latlng": [
+            62, 
+            10
+        ], 
+        "name": "Norway", 
+        "country_code": "NO", 
+        "capital": "Oslo"
+    },
+    {
+        "id":23,
+        "timezones": [
+            "Asia/Manila"
+        ], 
+        "latlng": [
+            13, 
+            122
+        ], 
+        "name": "Philippines", 
+        "country_code": "PH", 
+        "capital": "Manila"
+    },
+    {
+        "id":24,
+        "timezones": [
+            "Europe/Warsaw"
+        ], 
+        "latlng": [
+            52, 
+            20
+        ], 
+        "name": "Poland", 
+        "country_code": "PL", 
+        "capital": "Warsaw"
+    },
+    {
+        "id":25,
+        "timezones": [
+            "Europe/Lisbon", 
+            "Atlantic/Madeira", 
+            "Atlantic/Azores"
+        ], 
+        "latlng": [
+            39.5, 
+            -8
+        ], 
+        "name": "Portugal", 
+        "country_code": "PT", 
+        "capital": "Lisbon"
+    },
+    {
+        "id":26,
+        "timezones": [
+            "Asia/Seoul"
+        ], 
+        "latlng": [
+            37, 
+            127.5
+        ], 
+        "name": "South Korea", 
+        "country_code": "KR", 
+        "capital": "Seoul"
+    },
+    {
+        "id":27,
+        "timezones": [
+            "Europe/Madrid", 
+            "Africa/Ceuta", 
+            "Atlantic/Canary"
+        ], 
+        "latlng": [
+            40, 
+            -4
+        ], 
+        "name": "Spain", 
+        "country_code": "ES", 
+        "capital": "Madrid"
+    },
+    {
+        "id":28,
+        "timezones": [
+            "Europe/Stockholm"
+        ], 
+        "latlng": [
+            62, 
+            15
+        ], 
+        "name": "Sweden", 
+        "country_code": "SE", 
+        "capital": "Stockholm"
+    },
+    {
+        "id":29,
+        "timezones": [
+            "Europe/Zurich"
+        ], 
+        "latlng": [
+            47, 
+            8
+        ], 
+        "name": "Switzerland", 
+        "country_code": "CH", 
+        "capital": "Bern"
+    },
+    {
+        "id":30,
+        "timezones": [
+            "Europe/Istanbul"
+        ], 
+        "latlng": [
+            39, 
+            35
+        ], 
+        "name": "Turkey", 
+        "country_code": "TR", 
+        "capital": "Ankara"
+    },
+    {
+        "id":31,
+        "timezones": [
+            "Europe/London"
+        ], 
+        "latlng": [
+            54, 
+            -2
+        ], 
+        "name": "United Kingdom", 
+        "country_code": "GB", 
+        "capital": "London"
+    },
+    {
+        "id":32,
+        "timezones": [
+            "America/New_York", 
+            "America/Detroit", 
+            "America/Kentucky/Louisville", 
+            "America/Kentucky/Monticello", 
+            "America/Indiana/Indianapolis", 
+            "America/Indiana/Vincennes", 
+            "America/Indiana/Winamac", 
+            "America/Indiana/Marengo", 
+            "America/Indiana/Petersburg", 
+            "America/Indiana/Vevay", 
+            "America/Chicago", 
+            "America/Indiana/Tell_City", 
+            "America/Indiana/Knox", 
+            "America/Menominee", 
+            "America/North_Dakota/Center", 
+            "America/North_Dakota/New_Salem", 
+            "America/North_Dakota/Beulah", 
+            "America/Denver", 
+            "America/Boise", 
+            "America/Phoenix", 
+            "America/Los_Angeles", 
+            "America/Anchorage", 
+            "America/Juneau", 
+            "America/Sitka", 
+            "America/Metlakatla", 
+            "America/Yakutat", 
+            "America/Nome", 
+            "America/Adak", 
+            "Pacific/Honolulu"
+        ], 
+        "latlng": [
+            38, 
+            -97
+        ], 
+        "name": "United States", 
+        "country_code": "US", 
+        "capital": "Washington D.C."
+    },
+]
