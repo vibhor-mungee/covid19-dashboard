4ea231635db1c91b136cff758f2d9d02e23eb1bc
Vibhor Mungee <vibhor.mungee@6degreesit.com>
2020-04-03 16:43:04 +0530

    logo added

--- src/App.js
+++ src/App.js
@@ -9,9 +9,16 @@ function App() {
   // const[latLong,setLatLng]=useState([{lat:'20',lng:'77'}]);
   return (
     <div style={{ margin: '0px auto' }}>
+      <a href="https://www.6degreesit.com" target="_blank">
+        <img
+          src="https://www.6degreesit.com/wp-content/themes/6degree/images/logo-blk.png"
+          alt=""
+          style={{ position: 'fixed', zIndex: 1, background: "#0002", padding: 10 }}
+        />
+      </a>
       <Dashboard />
       <Map />
-      <div className="powered">Powered By <a href="https://www.6degreesit.com">6DegreesIT</a>. All rights reserved</div>
+      <div className="powered">Powered By <a href="https://www.6degreesit.com" target="_blank">6DegreesIT</a>. All rights reserved</div>
     </div>
   );
 }
